3a58aacfe6f24788ae7d428aeeea6e7c9b30b851
Dan315 <daniel827874735@gmail.com>
2022-11-16 19:09:05 -0600

    Maquetacion de pagina

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './Pages/login/login.component';
+import { InicioComponent } from './Pages/inicio/inicio.component';
+import { InventarioComponent } from './Pages/inventario/inventario.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'LogIn', component: LoginComponent },
+  { path: 'Inicio', component: InicioComponent },
+  { path: 'Inventarios/:inv', component: InventarioComponent },
+
+  { path: '**', redirectTo: 'LogIn', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './Pages/login/login.component';
+import { InicioComponent } from './Pages/inicio/inicio.component';
+import { InventarioComponent } from './Pages/inventario/inventario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    InicioComponent,
+    InventarioComponent
   ],
   imports: [
     BrowserModule,
